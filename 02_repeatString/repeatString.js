5995273efa2d56839b60581477093b374564012c
trevrus <4trevor@gmail.com>
2023-01-21 13:47:20 -0800

    Complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+    let result = '';
+    if (number < 0) return 'ERROR';
+    for (var i = 0; i < number; i++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
