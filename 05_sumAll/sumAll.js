6899c2465eb14d0857bd509131dadc5d9e3dd8a6
trevrus <4trevor@gmail.com>
2023-01-21 16:16:26 -0800

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    // check for errors
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return "ERROR";
+    }
+    // find the larger number
+    let lrgNum = num1 > num2 ? num1 : num2;
+    let smNum = lrgNum === num1 ? num2 : num1;
+
+    let sum = 0;
+    for (let i = smNum; i <= lrgNum; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
