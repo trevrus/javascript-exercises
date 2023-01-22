806320e7b47f25a963100c1ee161370c0d553173
trevrus <4trevor@gmail.com>
2023-01-22 10:30:09 -0800

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // round a number to 1 decimal place
+  let formula = (temp - 32) * 5 / 9;
+  return Math.round(formula * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let formula = temp * 9 / 5 + 32;
+  return Math.round(formula * 10) / 10;
 };
 
 // Do not edit below this line
