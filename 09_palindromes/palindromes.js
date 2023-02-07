5dae7c0c9b65dfbcd1606403002b57444593d89c
trevrus <4trevor@gmail.com>
2023-02-07 15:34:16 -0800

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // remove non-alphabetic characters and convert to lowercase
+    str = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    // convert to array, reverse, join back to string
+    const reverseString = str.split('').reverse().join('');
+    return reverseString === str;
 };
 
 // Do not edit below this line
