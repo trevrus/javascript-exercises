71d6c193d86d67d6f2c6457c91a7c6a0cab0ef7d
trevrus <4trevor@gmail.com>
2023-02-07 16:13:21 -0800

    Pass all tests
    I did not use recursion as copilot suggested. Instead
    I used an array and fetched the last two elements each loop.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    if (number < 0) return 'OOPS';
+    if (number === 0) return 0;
 
+    let arr = [];
+    for (let i = 0; i < number; i++) {
+        if (arr.length < 2) {
+            arr.push(1)
+        } else {
+            let next = arr[arr.length - 1] + arr[arr.length - 2];
+            arr.push(next);
+        }
+    }
+    console.log(arr[arr.length - 1]);
+    return arr[arr.length - 1];
 };
-
+// fibonacci(-2);
 // Do not edit below this line
 module.exports = fibonacci;
