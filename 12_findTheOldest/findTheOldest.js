44b8dcd67a3881f54f978c83ac73c9692774a42d
trevrus <4trevor@gmail.com>
2023-02-07 16:42:25 -0800

    Pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const oldestPerson = people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest);
+        const currentAge = getAge(current);
+        return oldestAge < currentAge ? current : oldest;
+    });
+    return oldestPerson;
 
 };
-
+function getAge(person) {
+    if (!person.yearOfDeath) {
+        return new Date().getFullYear() - person.yearOfBirth;
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
